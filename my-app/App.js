555e00da6616f41c91c5953930bfd137d575a2d1
Corvuscorax888 <137097044+Corvuscorax888@users.noreply.github.com>
2025-02-15 14:13:51 +0000

    receipts added

--- my-app/App.js
+++ my-app/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/first'
 import SecondScreen from './screens/second'
+import ReceiptScreen from './screens/third'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,10 +12,10 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Details">
+      <Stack.Navigator initialRouteName="Receipts">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={SecondScreen} />
-        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+        <Stack.Screen name="Receipts" component={ReceiptScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
