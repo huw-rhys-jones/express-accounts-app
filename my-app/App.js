4981cfce2c446294b882b138898ae6687d472ec2
Corvuscorax888 <137097044+Corvuscorax888@users.noreply.github.com>
2025-02-27 14:26:17 +0000

    added sign up

--- my-app/App.js
+++ my-app/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/first'
 import SecondScreen from './screens/second'
 import ReceiptScreen from './screens/third'
+import SignUpScreen from './screens/fourth'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -12,10 +13,11 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Receipts">
+      <Stack.Navigator initialRouteName="SignUp">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={SecondScreen} />
         <Stack.Screen name="Receipts" component={ReceiptScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
