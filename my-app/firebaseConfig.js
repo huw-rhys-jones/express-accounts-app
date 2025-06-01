01ffe0c2daf4afe09b2e56c84e743374a1a0a757
Huw Jones <huwjones37@gmail.com>
2025-06-01 02:23:42 +0100

    firebase creds

---
+++ my-app/firebaseConfig.js
@@ -0,0 +1,26 @@
+// firebaseConfig.js
+
+import { initializeApp } from 'firebase/app';
+import {
+  initializeAuth,
+  getReactNativePersistence
+} from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "express-accounts-73d38.firebaseapp.com",
+  projectId: "express-accounts-73d38",
+  storageBucket: "express-accounts-73d38.appspot.com", // <- fixed typo from ".firebasestorage.app"
+  messagingSenderId: "1061817175814",
+  appId: "1:1061817175814:web:0bb0d2496034887db49887",
+  measurementId: "G-JW54E87DRP"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export { auth };
