d5cbf5f8b1407d71a47d3d91f0fc979d04fbfa9e
Huw Jones <huwjones37@gmail.com>
2025-07-13 20:50:51 +0100

    trying to add google sign in

---
+++ my-app/auth/googleSignIn.js
@@ -0,0 +1,26 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { useEffect } from 'react';
+import { auth } from '../firebaseConfig';
+import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
+import Constants from 'expo-constants';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export function useGoogleSignIn() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: Constants.expoConfig.extra.GOOGLE_WEB_CLIENT_ID,
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential)
+        .then(() => console.log('Signed in with Google'))
+        .catch((error) => console.error('Google Sign-In error', error));
+    }
+  }, [response]);
+
+  return [request, promptAsync];
+}
