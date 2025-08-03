9db94c86907c99878f290e2726f2003d0293caf6
Huw Jones <huwjones37@gmail.com>
2025-08-04 00:56:31 +0100

    redirect uri updated

--- my-app/auth/googleSignIn.js
+++ my-app/auth/googleSignIn.js
@@ -1,5 +1,6 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
+import { makeRedirectUri } from 'expo-auth-session';
 import { useEffect } from 'react';
 import { auth } from '../firebaseConfig';
 import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
@@ -8,8 +9,14 @@ import Constants from 'expo-constants';
 WebBrowser.maybeCompleteAuthSession();
 
 export function useGoogleSignIn() {
+  const redirectUri = makeRedirectUri({
+    useProxy: true, // important for Expo Go
+  });
+
   const [request, response, promptAsync] = Google.useAuthRequest({
     clientId: Constants.expoConfig.extra.GOOGLE_WEB_CLIENT_ID,
+    scopes: ['openid', 'profile', 'email'],
+    redirectUri,
   });
 
   useEffect(() => {
