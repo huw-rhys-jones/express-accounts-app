59a4475103de01dfd57ba4ef4daf014a57d3268b
Huw Jones <huwjones37@gmail.com>
2025-08-10 01:37:10 +0100

    preparing for ios

--- my-app/auth/useGoogleSignIn.js
+++ my-app/auth/useGoogleSignIn.js
@@ -1,10 +1,10 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
-import { useEffect } from 'react';
-import { auth } from '../firebaseConfig';
-import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
-import { makeRedirectUri } from 'expo-auth-session';
-import Constants from 'expo-constants';
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { useEffect } from "react";
+import { auth } from "../firebaseConfig";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+import { makeRedirectUri } from "expo-auth-session";
+import Constants from "expo-constants";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -16,34 +16,35 @@ export function useGoogleSignIn(onSuccess) {
   const expoClientId = Constants.expoConfig.extra.GOOGLE_EXPO_CLIENT_ID;
   const androidClientId = Constants.expoConfig.extra.GOOGLE_ANDROID_CLIENT_ID;
   const webClientId = Constants.expoConfig.extra.GOOGLE_WEB_CLIENT_ID;
+  const iosClientId = Constants.expoConfig.extra.GOOGLE_IOS_CLIENT_ID;
 
   const [request, response, promptAsync] = Google.useAuthRequest({
+    iosClientId,
     androidClientId,
     webClientId,
     redirectUri,
-    scopes: ['openid', 'profile', 'email'],
+    scopes: ["openid", "profile", "email"],
   });
 
-  console.log('Redirect URI:', redirectUri);
+  console.log("Redirect URI:", redirectUri);
 
   useEffect(() => {
-    if (response?.type === 'success') {
+    if (response?.type === "success") {
       const id_token = response.authentication?.idToken;
       const access_token = response.authentication?.accessToken;
-  
+
       if (!id_token) {
         console.warn("No id_token received from Google");
         return;
       }
-  
+
       const credential = GoogleAuthProvider.credential(id_token, access_token);
-  
+
       signInWithCredential(auth, credential)
         .then(onSuccess)
-        .catch((error) => console.error('Google Sign-In error', error));
+        .catch((error) => console.error("Google Sign-In error", error));
     }
   }, [response]);
-  
 
   return [request, promptAsync];
 }
