cfc6c43140ece646c1ba6c6936e67dd1a686b481
Huw Jones <huwjones37@gmail.com>
2025-08-16 03:28:09 +0100

    trying to make old version work

--- my-app/app.config.js
+++ my-app/app.config.js
@@ -1,43 +1,49 @@
 import 'dotenv/config';
 
-export default {
-  expo: {
-    name: "Express Accounts",
-    slug: "express-accounts",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    newArchEnabled: true,
-    splash: {
-      image: "./assets/splash-icon.png",
-      resizeMode: "contain",
+export default ({ config }) => ({
+  ...config,
+  name: "Express Accounts",
+  slug: "express-accounts",
+  scheme: "com.caistec.expressaccounts", // 👈 ADD THIS
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: "com.caistec.expressaccounts",
+    buildNumber: "15"
+  },
+  android: {
+    package: "com.caistec.expressaccounts",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#ffffff"
-    },
-    ios: {
-      supportsTablet: true
-    },
-    android: {
-      package: "com.caistec.expressaccounts",
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      }
-    },
-    web: {
-      favicon: "./assets/favicon.png"
-    },
-    extra: {
-      eas: {
-        projectId: "5b149386-fb46-4d4d-8308-fde7bcff2f37"
-      },
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
     }
+  },
+  web: {
+    favicon: "./assets/favicon.png"
+  },
+  extra: {
+    eas: {
+      projectId: "5b149386-fb46-4d4d-8308-fde7bcff2f37"
+    },
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
+    GOOGLE_ANDROID_CLIENT_ID: process.env.GOOGLE_ANDROID_CLIENT_ID,
+    GOOGLE_IOS_CLIENT_ID: process.env.GOOGLE_IOS_CLIENT_ID
+
   }
-};
+});
